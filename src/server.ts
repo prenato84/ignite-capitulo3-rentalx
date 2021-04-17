c32cab3675199ddb5fc32de74bcebce266a34842
Paulo Castro <prenato84@gmail.com>
2021-04-17 15:36:06 -0300

    :sparkles: Add custom error handling

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import "reflect-metadata";
 
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 
+import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 
 import "./database";
@@ -13,5 +15,22 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+
+    next();
+  }
+);
 
 app.listen(3333, () => console.log("Server is running on port: 3333!"));
