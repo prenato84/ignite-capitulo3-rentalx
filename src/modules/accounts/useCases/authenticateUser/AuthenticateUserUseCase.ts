aba9ff81177f59b9eeb0ab2a7e54b9e17d879b86
Paulo Castro <prenato84@gmail.com>
2021-04-17 14:42:32 -0300

    Create user token and authenticate session

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,55 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    // Usuario existe
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Incorrect email or password!");
+    }
+
+    // Senha esta correta
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Incorrect email or password!");
+    }
+
+    // Gerar JTW
+    const token = sign({}, "abd44f99172b58564b506e5e1a165d53", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export { AuthenticateUserUseCase };
